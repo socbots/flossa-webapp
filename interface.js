ad73f25099d4cee5e96aa8d46d9a5419f616fbc4
Kristoffer Kuwaja <dahkaros@gmail.com>
2021-06-02 23:03:05 +0300

    modals on video, nice green buttons

--- interface.js
+++ interface.js
@@ -8,27 +8,33 @@ var modal = document.getElementById("canvasModal");
 // Get the button that opens the modal
 var btn = document.getElementById("canvasBtn");
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementById("canvasClose");
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-  modal.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
+        modal.style.display = "block";
+    }
+    // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
 /* Hide speak button on click */
-// Get the button
-var speakbutton = document.getElementById("speak");
+// Get the buttons
+var speakButton = document.getElementById("speak");
+var answerContainer = document.getElementById("answer-container");
 // Get the speak button container
-var speakcontainer = document.getElementById("speak-container");
-speakbutton.onclick = function () {
-  speakcontainer.style.display = "none"
+var speakContainer = document.getElementById("speak-container");
+// Get the question text container
+var questionContainer = document.getElementById("question");
+speakButton.onclick = function() {
+    speakContainer.style.display = "none"
+    answerContainer.style.display = "block"
+    questionContainer.style.display = "block"
+
 }
