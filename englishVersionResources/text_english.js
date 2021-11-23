eb8a0e9f5af252aa521c9444ec883e408d195ebb
Kristoffer Kuwaja <dahkaros@gmail.com>
2021-11-23 19:31:09 +0200

    added englishVersionResources

--- englishVersionResources/text_english.js
+++ englishVersionResources/text_english.js
@@ -1,5 +1,5 @@
-    /************* English *************/
-    /****** Manuscript for robot ******/
+/************* English *************/
+/****** Manuscript for robot ******/
 
 const intro = {
     "sentence": "Hello! My name is Alf and I help here at the dental clinic. Would you like to learn how to use dental floss? You can answer by speech or by pressing my screen.",
@@ -11,16 +11,16 @@ const cancel = "Have a nice day!"
 
 const sentence_video = {
     "sentence": "Nice. It is impossible to completely clean between your teeth with a standard toothbrush. \
-    Gingivitis and caries attack usually start from there. That is why it is recommended that you use dental \
-    floss at least once a day before you brush your teeth. Take a long enough string of dental floss and spin it \
-    around your fingers. Let the dental floss carefully follow the outline of your tooth and down into the gum. \
-    Pull the dental floss carefully up again and down on the other side of your tooth. Repeat this stage for each tooth, even the ones in the back."
+        Gingivitis and caries attack usually start from there. That is why it is recommended that you use dental \
+        floss at least once a day before you brush your teeth. Take a long enough string of dental floss and spin it \
+        around your fingers. Let the dental floss carefully follow the outline of your tooth and down into the gum. \
+        Pull the dental floss carefully up again and down on the other side of your tooth. Repeat this stage for each tooth, even the ones in the back."
 };
 
 const question_01 = {
     "sentence": "Now I have a question for you. Sometimes your gum starts to bleed when you use dental floss. \
-    What do you think is the cause of this? Say: Wrong technic, signs of gingivitis, or both. You can answer by speech of by using my screen.",
-    "answerA": "Wrong technic",
+        What do you think is the cause of this. Say Wrong technique, signs of gingivitis, or both. You can answer by speech or by using my screen.",
+    "answerA": "Wrong technique",
     "answerB": "Signs of gingivitis",
     "answerC": "Both"
 };
@@ -39,3 +39,8 @@ const question_02 = {
 };
 
 const outro = "Please discuss with your dentist or dental hygienist about what dental floss or toothbrush fits your needs the best. <break time='1s'/> Take care now and have a nice day."
+
+
+const sorry_repeat = '<speak> Sorry, I did not understand you? <break time="1s"/></speak>'
+
+const feedback_container_before = "What I heard: "
